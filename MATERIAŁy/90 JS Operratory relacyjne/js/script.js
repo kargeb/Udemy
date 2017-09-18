857d5c736dda2b3ffdf1eba6eb58ce5471242046
Praca <karol.gebarowski@gmail.com>
2017-09-18 14:52:51 +0200

    js start

---
+++ MATERIAŁy/90 JS Operratory relacyjne/js/script.js
@@ -0,0 +1,26 @@
+/*
+ * 
+ *  OPERATORY RELACYJNE (porównania)
+ *  
+ *  === - porównuje czy wartości dwóch zmiennych są IDENTYCZNE
+ *  == - porównania
+ *  = - operator przypisania
+ *  
+ *  != - porównuje czy dwie wartości są od siebie różne
+ *  !== 
+ *  
+ *  >
+ *  <
+ *  >=
+ *  <=
+ *   
+ */
+
+var a = -4,
+    b = 3;
+    
+alert(a < b);    
+
+
+
+
---
+++ MATERIAŁy/94 Zasieg zmiennych/js/script.js
@@ -0,0 +1,19 @@
+/*  
+ *  Funkcje - zasięg zmiennych
+ *  
+ *  zmienne globalne i lokalne
+ *  
+ */
+
+var a = 5;
+
+function test(a) 
+{
+    //var a;
+    a = 3;
+ 
+}
+
+test();
+
+alert(a);
