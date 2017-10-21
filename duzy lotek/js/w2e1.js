536e2ecb8ea899dd0f7c894663eb0880e7f95eb6
Karol Gebarowski <karol.gebarowski@gmail.com>
2017-10-21 19:29:23 +0200

    pola formulartza zaczenete

--- duzy lotek/js/w2e1.js
+++ duzy lotek/js/w2e1.js
@@ -1,15 +1,10 @@
 console.log("poka");
 
 var button = document.querySelector("button"),
-    poka = document.querySelector("#ukryty");
-
-console.log(button, poka);
+    poka = document.querySelectorAll("div");
 
 button.onclick = function(){
     
-    if(button.style.display = "none") {
-        button.classList.remove(""))
-    } else {
-        button.style.display = "none"
-    }
+    poka[1].classList.toggle("ukryj");
+    
 }
---
+++ duzy lotek/js/w2e2.js
@@ -0,0 +1,1 @@
+console.log("formularz psa");
