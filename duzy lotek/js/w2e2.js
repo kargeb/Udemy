8aed8eb1052dbf275c8e8e831e06171d1828e200
Praca <karol.gebarowski@gmail.com>
2017-10-25 14:40:15 +0200

    formularze dalej

--- duzy lotek/js/w2e2.js
+++ duzy lotek/js/w2e2.js
@@ -1,7 +1,24 @@
+(function(){
+
 console.log("formularz psa");
 
-var fields = document.querySelectorAll("[name]"),
-    button = document.querySelector("button");
+var entryForm = document.querySelector("#entryForm"),
+    fields = entryForm.querySelectorAll("[name]"),
+    button = entryForm.querySelector("button");
 
 console.log(fields);
 console.log(button);
+
+
+entryForm.addEventListener("submit", function(e){
+    
+    e.preventDefault();
+    console.log("poszlo submit");
+    
+    fields.forEach(function(){
+        console.log(this);
+    });
+    
+}, false);
+    
+})();
