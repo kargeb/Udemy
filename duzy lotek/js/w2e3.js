0fb429e81e28d08b82a3964b200a0c3a4e4f7257
Praca <karol.gebarowski@gmail.com>
2017-10-27 09:28:49 +0200

    setTime PIEKNIE rozjebany

--- duzy lotek/js/w2e3.js
+++ duzy lotek/js/w2e3.js
@@ -2,38 +2,66 @@
  
 console.log("dupa dupa");
 
-var zygor = document.querySelector("#zygor"),
-    button = document.querySelector("button"),
+var zygorInt = document.querySelector("#interval"),
+    zygorTime = document.querySelector("#timeout"),
+    buttonInt = document.querySelector("button[name='int']"),
+    buttonTime = document.querySelector("button[name='time']"),
     counter = 10;
     
-    console.log(zygor);
+    console.log(zygorInt, zygorTime, buttonInt, buttonTime);
     
-function execute() {
-    button.style.color = "white";
-    button.style.background = "black"
+function executeInt() {
+    buttonInt.style.color = "white";
+    buttonInt.style.background = "black"
 }     
     
-function nadupcaj(fun) {
+function executeTime() {
+    buttonTime.style.color = "yellow";
+    buttonTime.style.background = "blue"
+}         
+    
+function nadupcajInt(fun) {
     var stop = setInterval(function(){
         counter--;
-        zygor.textContent = counter;
+        zygorInt.textContent = counter;
         if(!counter){
             console.log("dupa dfupa dupa udfpa");
             clearInterval(stop);
             fun();
+            counter = 10;
             
         }
         
     },500);
 }    
-        
+
     
-button.onclick = function(){
     
-    nadupcaj(execute);
+buttonInt.onclick = function(){
+    
+    nadupcajInt(executeInt);
     
     console.log("koniec");
 }    
+
+function nadupcajTime(fn, time, count) {
+    
+    if(count+1){
+        setTimeout(function(){
+            zygorTime.textContent = count;
+            console.log(count);
+            count--;
+            nadupcajTime(fn, time, count);
+        }, time);
+    } else {
+        fn();
+    }
+
+}
+
+buttonTime.onclick = function(){
+    nadupcajTime(executeTime, 1000, 9);
+}
     
  })();
 
