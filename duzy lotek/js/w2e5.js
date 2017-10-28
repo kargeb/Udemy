bedba0ab772b2b66520af4248ac6349ce676d1b7
Karol Gebarowski <karol.gebarowski@gmail.com>
2017-10-28 22:39:38 +0200

    pieknie panie, 2 tydz rozdupcony

--- duzy lotek/js/w2e5.js
+++ duzy lotek/js/w2e5.js
@@ -1,38 +1,72 @@
-console.log("callback");
-
-var button = document.querySelector("button");
-var counter = 0;
-
-function start() {
-    console.log("dupa");
-}
-
-//var halt = setTimeout(start, 1000);
+//console.log("callback");
 //
-//function stop(halt) {
-//    clearTimeout(halt);
-//    console.log("stop funkcja");
+//var button = document.querySelector("button");
+//var counter = 0;
+//
+//function start() {
+//    console.log("dupa");
 //}
+//
+//
+//var hold = null;
+//
+//function debounce(fn, time) {
+//
+//    return function (){
+//    
+//    clearTimeout(hold);
+//    hold = setTimeout(fn, time);
+//    };
+//}
+//
+//var fun = debounce(start, 1000);
+//
+//button.onclick = function(){
+//        fun();
+//    }
 
-var hold = null;
 
-function debounce(fn, time) {
+(function() {
 
-    console.log(time);
+    var span = document.querySelectorAll("span"),
+        funScroll = 0,
+        funResize = 0;
     
-    return function (){
-    
-    clearTimeout(hold);
-    hold = setTimeout(fn, time);
-    console.log("hold: " + hold);
-    };
-}
+    function debounce(fn, time) {
+        // docelowo powinna być wywoływana
+        // funkcja fn w ten sposób fn()
+        var hold = null;
+        // część kodu tutaj
 
-var fun = debounce(start, 1000);
-
-button.onclick = function(){
-        fun();
+        return function() {
+            // pozostały kod tutaj
+        clearTimeout(hold);
+        hold = setTimeout(fn, time);
+        };
     }
 
+    var handleScroll = debounce(function() {
+        console.log("Scrollujemy!");
+        funScroll++;
+        span[0].textContent = "Scrollujemy! " + funScroll;
+    }, 200);
+
+    // w tym miejscu pod handleScroll
+    // powinna być nowa funkcja
+
+    // Przypisanie zdarzenia "scroll"
+    window.addEventListener("scroll", handleScroll, false);
+
+    // Możesz również przetestować funkcję
+    // ze zdarzeniem "resize" skalując okno
+    var handleResize = debounce(function() {
+        console.log("Zmieniamy rozmiar okna!");
+        funResize++;
+        span[1].textContent = "Zmieniamy rozmiar okna!" + funResize;
+    }, 100);
+
+    window.addEventListener("resize", handleResize, false);
+
+})();
 
 
