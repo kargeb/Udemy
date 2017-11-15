21de14f7c21056ca55cdacee5966cb2dfff9f00c
Karol Gebarowski <karol.gebarowski@gmail.com>
2017-11-15 21:43:15 +0100

    reaktywacja

--- duzy lotek/js/jquery_testy.js
+++ duzy lotek/js/jquery_testy.js
@@ -1,1 +1,12 @@
-console.log("jquery");
+console.log("jquery dupa");
+
+console.log($);
+
+
+var p = $("P"),
+    klasa = $(".line"),
+    button = $("button");
+
+console.log(p);
+console.log(klasa);
+console.log(button);
