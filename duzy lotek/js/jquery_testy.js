52064394c5c6282d05a0b60251d87b55fb299a12
Praca <karol.gebarowski@gmail.com>
2017-11-16 13:23:58 +0100

    pomalu jquery

--- duzy lotek/js/jquery_testy.js
+++ duzy lotek/js/jquery_testy.js
@@ -5,8 +5,12 @@ console.log($);
 
 var p = $("P"),
     klasa = $(".line"),
-    button = $("button");
+    button = $("button"),
+    pierw = $(".pierw");
 
 console.log(p);
 console.log(klasa);
 console.log(button);
+console.log(pierw);
+console.log($(pierw));
+
