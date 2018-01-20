ab6b3806b083cf429128cef98588aa0e740e62f3
Karol Gebarowski <karol.gebarowski@gmail.com>
2018-01-20 19:47:41 +0100

    4 in the middle

--- duzy lotek/js/w4e4.js
+++ duzy lotek/js/w4e4.js
@@ -3,6 +3,32 @@
     $(document).ready(function() {
     
         console.log("dupa 4");
+        
+        var btn = $(".btn"),
+            output = $("#output"),
+            names = "";
+        
+        
+        
+        btn.on("click", function(){
+            
+                $.getJSON("http://code.eduweb.pl/bootcamp/users/", function(data){
+
+                $(data).each(function(i, elem){
+
+                $("<li></li>", {
+                    text: elem.name
+                }).appendTo(output);    
+
+                    
+                    
+            });
+                
+            //output.html(names);
+                
+            })
+            
+        })
 
     });
     
