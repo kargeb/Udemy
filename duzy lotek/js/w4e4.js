3e68b06aa070d4b86eb5022f9d56cb482ad6a4d0
Karol Gebarowski <karol.gebarowski@gmail.com>
2018-01-24 20:57:41 +0100

    4 exercise done

--- duzy lotek/js/w4e4.js
+++ duzy lotek/js/w4e4.js
@@ -6,55 +6,72 @@
         
         var btn_basic = $("#btn_basic"),
             btn_advanced = $("#btn_advanced"),
-            output1 = $("#output1"),
-            output2 = document.getElementById("output2"),
-            source = document.getElementById("tpl").innerHTML,
+            btn_list = $("#btn_list"),
+       //     output1 = $("#output1"),
+            output = document.getElementById("output"),
+            sourceList = document.getElementById("tpl").innerHTML,
+            sourceTable = document.getElementById("tpl2").innerHTML,
             snip1, snip2;
         
         
         
         btn_basic.on("click", function(){
             
+            $(output).empty();
+            
             $.getJSON("http://code.eduweb.pl/bootcamp/users/", function(data){
+                
                 
                 $(data).each(function(i, elem){
 
                     snip2 = "username: " + elem.username + "   |  email: " + elem.email + '   |  tel.: ' + elem.phone;    
 
                     snip1 = document.createElement("li");
-
                     snip1.innerHTML = "<b>" + elem.name + "</b><br>" + snip2;    
 
-                    output1.append(snip1);  
-
+                    output.append(snip1);  
                 });        
            });
         });
         
         btn_advanced.on("click", function(){
             
+            $(output).empty();
+            
             $.getJSON("http://code.eduweb.pl/bootcamp/users/", function(data){
                 
-            var template = Handlebars.compile(source);
+                var template = Handlebars.compile(sourceList);
+
+                var dane  = {
+                    pracownicy: data
+                }  
+
+                var html = template(dane);
+
+                output.innerHTML = html;    
                 
-            var dane = {
-                imie: data[1].name,
-                nazwisko: data[1].username
-            }    
-            
-            var html = template(dane);
-                
-            output2.innerHTML = html;    
-                
-            });    
-                    
+            });           
         });
         
-        
-        
-        
-        
+        btn_list.on("click", function(){
+           
+            $(output).empty();
+            
+            $.getJSON("http://code.eduweb.pl/bootcamp/users/", function(data){
+                
+                var template = Handlebars.compile(sourceTable);
 
+                var dane  = {
+                    pracownicy: data
+                }  
+
+                var html = template(dane);
+
+                output.innerHTML = html;    
+                
+            });                
+        });
+        
     });
     
 })(jQuery);
