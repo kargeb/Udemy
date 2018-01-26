bdd4e6d5971011185c513ea07976660cbd5aa338
Karol Gebarowski <karol.gebarowski@gmail.com>
2018-01-26 14:31:07 +0100

    pluginy pocz

--- duzy lotek/js/w4e4.js
+++ duzy lotek/js/w4e4.js
@@ -52,7 +52,7 @@
         });
         
     function show(source, data) {
-
+        
         var template = Handlebars.compile(source);
 
         var dane  = {
---
+++ duzy lotek/js/w4e5.js
@@ -0,0 +1,35 @@
+(function($, window, document, undefined) {
+   
+    $.fn.highlight = function(userOptions) {
+        
+        var defaults = {
+            shortColor: "red",
+            longColor: "green" 
+        };
+        
+        var options = $.extend({}, defaults, userOptions);
+        
+        console.log(options);
+        
+        return this.each(function(){
+           
+            var that = $(this),
+                text = that.text();
+            
+            if(text.length > 100) {
+                that.css("color", options.longColor);
+            } else {
+                that.css("color", options.shortColor);
+            }
+        });
+    };
+    
+    $(document).ready(function(){
+        
+        $("p").highlight({
+          // shortColor: "blue" 
+        });
+        
+    })
+    
+})(jQuery, window, document);
