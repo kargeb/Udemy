cdb7d81e3a704d92ca5ab3c93aa01a50b4b24641
Karol Gebarowski <karol.gebarowski@gmail.com>
2018-01-31 19:10:54 +0100

    symbols

--- EcmaSript_6/app/examples/27/start/js/scripts.js
+++ EcmaSript_6/app/examples/27/start/js/scripts.js
@@ -30,6 +30,7 @@ console.log(person[hidden]);  // UWAGA !! BEZ ".' !!!
 
 // Praltyczne zastosowanie:
 // ZROBIMY COS NA WZOR PRYWATNEJ MOETODY W KLASIE !
+// &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 const FORMAT = Symbol("format()");
 
  class Person {
@@ -39,7 +40,7 @@ const FORMAT = Symbol("format()");
          this.lastName = lastName;
      }
 
-     sayHello() {
+     sayHello() {       // BARDZO WAZNY ZAPIS ! Specyficzny ! Latwo sie jebnac
          return this[FORMAT]( `${this.firstName} ${this.lastName}` );  // BEZ "." !!
      }
 
@@ -47,9 +48,20 @@ const FORMAT = Symbol("format()");
          return text.toUpperCase();
      }   
  }
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
 let person1 = new Person("Jan", "Kowalski");
 
 console.log( person1.sayHello() );
 
-// DOKOCNZ !!!!!!!!!!!!!!!!!!
+// Uwaga w tym momencie udalo nam sie TEORETYCZNIE utworzyć METODĘ PRYWATNĄ !
+// UWAGA TEORTYCZNIE no bo w tym pliku w którym jesteśmy
+// MAMY DOSTĘP DO TEJ METODY W TEN SPOSÓB:
+//  person1[FORMAT]()
+// Lecz normalnie to tego symnolu FORMAT nie bedzie mozna miec dostepu. WSZYSTKO ROZCHODZI SIĘ O MODUŁY.
+// no bo wylacznie TEEEN OBSZAR &&&&&&&& bedzie mial dostep do Sybmolu FORMAT
+// a jak sobie zrobomy na końcu pliku
+exports default Person
+// To topiero w drugim pliku tworzylibyśmy obiekty tej klasy ! 
+// No i w zupełnie innym module NIE BEDZIE NIKT MIEC DOSTEPU WLASNIE DO person1[FORMAT]()
+// Nie ograniam troche idei tego symbolu ale mysle przyjdzie z czasem
--- EcmaSript_6/app/examples/28/start/js/scripts.js
+++ EcmaSript_6/app/examples/28/start/js/scripts.js
@@ -1,8 +1,47 @@
-const hidden = Symbol("hidden");
+// 31. Metody symboli
+
+//Jest kilka metod dostępnych dla symboli
+// W tym samym pliku ktos moze sie niby odniesc do naszego symbolu ALE NIE MA MOZLIWOSCI GO NADPISAC
+
+//const hidden = Symbol("hidden");
+// UWAGA poza swyklym symbolem jak u góry, który dostępny jest tylko w tym pliku MOZNA UTWORZYĆ SPEJCALNY SYMBOL który będzie w specjjalnym GLOBALNYM REJESTRZE i będzie można się do niego odwołać W KAŻDYM MIEJSCU W PROGRAMIE niezaleznie w jakim pliku byśmy byli
+
+//---1--- Symbol.for("hidden");
+(function() {
+    
+Symbol.for("hidden");   // normalnie nie ma opcji zebysmy sie do tej zmiennej odwolali, ale wlasnie dzieji temu globalnemu rejestrowi MOZEMY !
+// METODA ta sprawdza czy pod podabnnym strigniem  ISTNIEJE JUZ TAKI KLUCZ i jeżeli nie TO GO TWORZY I ZA KAŻDYM ODWOŁANIEM SIĘ 
+//  [Symbol.for("hidden")]  będziemy mieć dostęp do tego klucza w dowolnym miejscu w kodzie ! No i mozna sobie pod tymi symbolami trzymac coś czego w żaden sposób nie będzie można zmienić ani nadpisać    
+})();
+
+// ---2--- 
+// Możemy sprawdzić co zostało użyte (jaki strong) podczas tworzenia danego symbolu. Jemu sie to jeszcze nigdy nie przydało ale można to zrobić 
+// za pomocą metody  Symbol.keyFor(hidden)
+const hidden2 = Symbol.for("hidden2");
 
 let person = {
-    [hidden]: "123jkasdKhasdf$901-123",
+    [Symbol.for("hidden2")]: "123jkasdKhasdf$901-123",
     getSecret() {
-        return this[hidden];
+        return this[Symbol.for("hidden2")];
     }
 };
+
+console.log( person.getSecret() );  // "123jkasdKhasdf$901-123"
+console.log( Symbol.keyFor(hidden2) );  // hidden2   <--- tu jest wlasnie wywolanie stringu tworzącego Symbol
+
+// ---3---
+// PONIŻSZA METODA ZWRACA WSZYSTKIE WŁAŚCIWOŚCI OBIEKTU KTÓEGO DO NIEJ PRZEKAŻEMY !!! (CHyba zajebista mi sie zdaje)
+console.log( Object.getOwnPropertyNames(person) );
+// Zwraca ona w naszym wypdaku jedynie "getSecret" CZYLI TAK SAMO JAK PETLA FOR NIE WIDZI SYMBOLI !
+// UWAGA! No i teraz od ES6 mamy metode KTORA WYPISZE WSZYSTKIE SYMBOLE JAKIE POSIADA OBIEKT !
+console.log( Object.getOwnPropertySymbols(person) );  //    [Symbol(hidden2)]
+// Dostajemy TABLICE symboli i np jak odwolamy sie do pierwszego elementu TO TAK JAKBYSMY ODWOLYWALI SIE PROSTO DO TEGO SYMBOLU np:
+var zmienna = Object.getOwnPropertySymbols(person);
+console.log( zmienna[0] );
+
+console.log( Symbol.for("hidden2") === zmienna[0] );  // TRUE !!!
+// I WLASNIE DLATEGO RZECZY DODANE DO OBIEKTÓW NIE SĄ PRYWATNE W !00% BO ZAWSZE MOZNA SOBIE TA METODA WYWOLAC, pobrac wszystkie symbole i za ich pomocą WGRYŹĆ się do obiektu !
+
+// KIEDY SYMBOLE SIE PRZYDAJA ?
+// Podbno bardzo na klasach gdzie mozna stworzyc juz swego rodzaju prywatność
+// A juz najbardziej są pomocne w przypadku tzw WELL-KNOWN SYMBOLS czyli SYMBOLI WBUDOWANYCH co oferuje nam juz JS (kolejna lekcja)
