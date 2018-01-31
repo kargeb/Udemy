bb531ef74d90a0d495624e2d2de0e1344b8f3e07
Karol Gebarowski <karol.gebarowski@gmail.com>
2018-01-31 21:40:40 +0100

    symbolss

--- EcmaSript_6/app/examples/29/start/js/scripts.js
+++ EcmaSript_6/app/examples/29/start/js/scripts.js
@@ -1,4 +1,9 @@
-/*
+// 32. Symbole wbudowane 
+
+// Jesli byly watpliwosci czy symbole sa przydatme to tutaj przkonasz sie ze TAK ! No i mimo ze nie bedziesz na codzien z wszystkich z nich korzystal TO NIE SPOSÓB SOBIE WYOBRAZIĆ ŻE KTOŚ MOŻE NIE WIEDZIEĆ O ICH ISTENIENIU I WYKORZYSTANIU !!!
+//Najważniejsze w Symbolacvh to to ŻE WARTOŚĆ W NICH JEST ZAWSZE UNIKALNA ! I ES6 daje nam wlasnie takie symbole wbydowane
+
+/*   WSZYSTKIE WELL-KONWN SYMBOLS !
 
 - Symbol.hasInstance
 - Symbol.toPrimitive
@@ -11,11 +16,11 @@
 
 */
 
-class Person {
+class Person {  // KLASA ABSTRAKCYJNA DZIĘKI LINIJCE Z new.target
 
     constructor(firstName, lastName) {
 
-        if(new.target === Person) {
+        if(new.target === Person) {     // DZIĘKI TEMU JEST TO KLASA ABSTRAKCYJNA
             throw new Error("Klasa Person nie może być użyta bezpośrednio.");
         }
 
@@ -43,3 +48,10 @@ class Employee extends Person {
 }
 
 let employee1 = new Employee("Jan", "Kowalski", "programista");
+
+// TO SĄ KURWA HARDOKOROWE RZECZY, i korzstają z nich raczej sami madzy level 70 ALE mozna to chyba ogranać !
+// Chodzi o to ze ES6 Dał możliwość grzebania W SAMYM INTERPETERZE JS ! Np za pomoca PROXY i Symboli wlasnie
+// No i pierwszy symbol  Symbol.hasInstance  moze sam ustawic czy dany obiekt BEDZIE instancja danej klasy CZY TEZ NIE !
+
+// PIERDOLE !!! Za duzy Hardkor ...
+// JEDEN SYMBOL JEST ZAJEBITY -->  Symbol.iterator  ALE O NIM BEDA OSOBNE LEKCJE I Z NIEGO WLASNIE BEDZIE SIE NAJCZESCIEJ KORZYSTAC !
